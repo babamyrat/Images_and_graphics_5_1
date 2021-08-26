75503a6cf9fd06c227589cc86fb79895217f1600
Admin <tmstudentler@gmail.com>
2021-08-26 14:44:19 +0300

    Step 1 resource drawable

--- app/src/main/java/com/example/images_and_graphics_5_1/MainActivity.java
+++ app/src/main/java/com/example/images_and_graphics_5_1/MainActivity.java
@@ -11,29 +11,18 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ConstraintLayout constraintLayout;
+   private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a ConstraintLayout in which to add the ImageView
-        constraintLayout = new ConstraintLayout(this);
+        imageView = findViewById(R.id.imageView2);
 
-        // Instantiate an ImageView and define its properties
-        ImageView i = new ImageView(this);
-        i.setImageResource(R.drawable.ic_launcher_background);
-        i.setContentDescription(getResources().getString(R.string.my_image_desc));
+        Resources res = this.getResources();
+        Drawable myImage = ResourcesCompat.getDrawable(res, R.drawable.ic_launcher_background, null);
 
-        // set the ImageView bounds to match the Drawable's dimensions
-        i.setAdjustViewBounds(true);
-        i.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        // Add the ImageView to the layout and set the layout as the content view.
-        constraintLayout.addView(i);
-        setContentView(constraintLayout);
+        imageView.setImageDrawable(myImage);
     }
 }
