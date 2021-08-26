1987070401b186250703a35e59a889f7981dc2d8
Admin <tmstudentler@gmail.com>
2021-08-26 13:06:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/images_and_graphics_5_1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.images_and_graphics_5_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+// Create a ConstraintLayout in which to add the ImageView
+        constraintLayout = new ConstraintLayout(this);
+
+        // Instantiate an ImageView and define its properties
+        ImageView i = new ImageView(this);
+        i.setImageResource(R.drawable.ic_launcher_background);
+        i.setContentDescription(getResources().getString(R.string.my_image_desc));
+
+        // set the ImageView bounds to match the Drawable's dimensions
+        i.setAdjustViewBounds(true);
+        i.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // Add the ImageView to the layout and set the layout as the content view.
+        constraintLayout.addView(i);
+        setContentView(constraintLayout);
+    }
+
+}
