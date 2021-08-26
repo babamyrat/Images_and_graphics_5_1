c5d75c2ff9abc0047a1b072922214e71cfcc44ce
Admin <tmstudentler@gmail.com>
2021-08-26 14:40:13 +0300

    Step 1 resource images

--- app/src/main/java/com/example/images_and_graphics_5_1/MainActivity.java
+++ app/src/main/java/com/example/images_and_graphics_5_1/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.res.ResourcesCompat;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -12,18 +11,29 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout constraintLayout;
 
-    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView2);
+        // Create a ConstraintLayout in which to add the ImageView
+        constraintLayout = new ConstraintLayout(this);
 
-        Resources res = this.getResources();
-        Drawable myImage = ResourcesCompat.getDrawable(res, R.drawable.ic_launcher_background, null);
+        // Instantiate an ImageView and define its properties
+        ImageView i = new ImageView(this);
+        i.setImageResource(R.drawable.ic_launcher_background);
+        i.setContentDescription(getResources().getString(R.string.my_image_desc));
 
-        imageView.setImageDrawable(myImage);
+        // set the ImageView bounds to match the Drawable's dimensions
+        i.setAdjustViewBounds(true);
+        i.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // Add the ImageView to the layout and set the layout as the content view.
+        constraintLayout.addView(i);
+        setContentView(constraintLayout);
     }
 }
